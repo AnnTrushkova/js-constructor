85064550f4f37a20fa6e91aff9c12d7e04a16536
Anna Trushkova <anna_trushkova.94@mail.ru>
2020-08-24 21:00:22 +0300

    a constructor is created, the app is uploaded to the hosting

--- src/model.js
+++ src/model.js
@@ -1,24 +1,34 @@
-import image from './assets/helpbox-contact.jpg'
+// import image from './assets/helpbox-contact.jpg'
+import image from './assets/english.jpg'
 import {TitleBlock, ImageBlock, TextBlock, TextColumnBlock} from './classes/blocks'
+import {css} from "./utils"
 
 export const model = [
 
-    new TitleBlock('Test Title', {
-        tag: 'h1',
-        styles: 'background: #0552d4; color: #fff; text-align: center; padding: 15px 0;'
+    new TitleBlock('Курсы английского языка в Минске', {
+        tag: 'h2',
+        styles: css({
+            background: '#111111',
+            color: '#fff',
+            'text-align': 'center',
+            padding: '1.5 rem',
+        })
     }),
     new ImageBlock(image, {
         alt: 'my img',
-        imageStyles: 'width: 100%'
+        imageStyles: 'width: 100%; height: 400px'
     }),
     new TextColumnBlock([
-        '1 text dfkj skdjksdj lkjsd sdkasd kjasd',
-        '2 text dfkj skdjksdj lkjsd sdkasd kjasd',
-        '3 text dfkj skdjksdj lkjsd sdkasd kjasd',
+        'Языковые курсы для взрослых',
+        'Языковые курсы для детей',
+        'Онлайн-обучение',
     ], {
-        styles: 'background: #3c5c09; color: white; text-align: center; padding: 3rem; '
+        styles: 'background: #f8de05; color: 2d1e1e; font-weight: bold; text-align: center; padding: 3rem; '
     }),
-    new TextBlock('Lorem ipsum dolor sit amet, consectetur adipisicing elit.', {
+    new TextBlock('Наши курсы английского языка доступны для начинающих с нуля и для самых продвинутых студентов. ' +
+        'Обучаться можно в групповом, индивидуальном или VIP-формате. Несомненным преимуществом является постоянная ' +
+        'дополнительная практика: бесплатные занятия с носителями языка из Великобритании и США, языковые клубы и ' +
+        'проекты Streamline Club. ', {
         styles: 'background: #dbdbdb47; color: `black`; padding: 3rem;'
     })
 
